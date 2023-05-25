b83ab3c48dcb6164fafc0aabbe3a9ec74e1d00ae
John Cote <johncote61598@gmail.com>
2023-05-25 14:59:39 -0400

    did GET request for all planets. All images now showing in each photo gallery

--- index.js
+++ index.js
@@ -76,3 +76,124 @@ const getVenus = async () => {
 } 
 
 getVenus() 
+
+// GET request for earth's photo gallery 
+const getEarth = async () => {  
+    const egContainer = document.querySelector('#earth-gallery-container')  
+    try{
+        const response = await fetch('http://localhost:3000/earth-images') 
+        if (response.ok){
+            const respJson = await response.json() 
+            respJson.forEach(earth => {
+                const earthImg = document.createElement('img') 
+                earthImg.src = earth.image  
+                egContainer.append(earthImg) 
+            })
+        }
+    } catch(error){
+        console.log(error)  
+    }
+} 
+
+getEarth() 
+
+// GET request for mars photo gallery 
+const getMars = async () => {  
+    const mgContainer = document.querySelector('#mars-gallery-container')  
+    try{
+        const response = await fetch('http://localhost:3000/mars-images')  
+        if (response.ok){
+            const respJson = await response.json() 
+            respJson.forEach(mars => {
+                marsImg = document.createElement('img')  
+                marsImg.src = mars.image  
+                mgContainer.append(marsImg)  
+            })
+        }
+    } catch(error){
+        console.log(error) 
+    }
+}   
+
+getMars() 
+
+// GET request for jupiter photo gallery 
+const getJupiter = async () => {   
+    const jgContainer = document.querySelector('#jupiter-gallery-container') 
+    try{
+        const response = await fetch('http://localhost:3000/jupiter-images')  
+        if (response.ok){
+            const respJson = await response.json() 
+            respJson.forEach(jupiter => {
+                const jupiterImg = document.createElement('img')  
+                jupiterImg.src = jupiter.image   
+                jgContainer.append(jupiterImg)  
+            })
+        } 
+    } catch(error){
+        console.log(error)  
+    }
+} 
+
+getJupiter() 
+
+// GET request for saturn photo gallery 
+const getSaturn = async () => {  
+    const sgContainer = document.querySelector('#saturn-gallery-container')
+    try{
+        const response = await fetch('http://localhost:3000/saturn-images')  
+        if (response.ok){
+            const respJson = await response.json() 
+            respJson.forEach(saturn => {
+                const saturnImg = document.createElement('img') 
+                saturnImg.src = saturn.image 
+                sgContainer.append(saturnImg)  
+            })
+        }
+    } catch(error){
+        console.log(error)
+    }
+}  
+
+getSaturn() 
+
+// GET request for uranus' photo gallery 
+
+const getUranus = async () => {
+    const ugContainer = document.querySelector('#uranus-gallery-container')
+    try {
+        const response = await fetch('http://localhost:3000/uranus-images')
+        if (response.ok) {
+            const respJson = await response.json()
+            respJson.forEach(uranus => {
+                const uranusImg = document.createElement('img')
+                uranusImg.src = uranus.image
+                ugContainer.append(uranusImg)
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getUranus() 
+
+// GET request for neptune 
+const getNeptune = async () => {
+    const ngContainer = document.querySelector('#neptune-gallery-container')
+    try {
+        const response = await fetch('http://localhost:3000/neptune-images')
+        if (response.ok) {
+            const respJson = await response.json()
+            respJson.forEach(neptune => {
+                const neptuneImg = document.createElement('img')
+                neptuneImg.src = neptune.image
+                ngContainer.append(neptuneImg)
+            })
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+getNeptune()  
